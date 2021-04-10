3ca3e6db7e302cf2608f65664726606ff0f08128
Mahika Jain <mj2249@nyu.edu>
2021-04-09 20:49:36 -0400

    Add credit card python file

---
+++ CreditCardFraudDetection.py
@@ -0,0 +1,11 @@
+# %%
+#Importing the libraries to be used:
+import numpy as np
+import pandas as pd
+from sklearn import linear_model
+import matplotlib.pyplot as plt
+%matplotlib inline
+# %%
+df = pd.read_csv('creditcard.csv')
+print(df.shape)
+# %%
